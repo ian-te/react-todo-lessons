426595f94afc84ae9d2bac518682522eaf6ff5f0
Yan Te <yan.te@team.wrike.com>
2017-02-01 10:14:56 +0300

    todo reducer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import deepFreeze from 'deep-freeze';
 import expect from 'expect';
 
-const toggleTodo = (todo) => {
-    return Object.assign({}, todo, {
-        completed: !todo.completed
-    })
-}
+const todos = (state = [], action) => {
+    switch (action.type) {
+    case 'ADD_TODO':
+        return  [
+            ...state,
+            {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }
+        ]
+        break;
+    default:
+        return state;
 
-const testToggleTodo = () => {
-    const todoBefore = {
-        id: 0,
-        name: 'test todo',
-        completed: false
-    };
-    const todoAfter = {
-        id: 0,
-        name: 'test todo',
-        completed: true
     }
-    deepFreeze(todoBefore)
-    expect(toggleTodo(todoBefore))
-        .toEqual(todoAfter)
 }
 
-testToggleTodo()
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn something cool'
+    };
+
+    const stateAfter = [{
+        id: 0,
+        text: 'Learn something cool',
+        completed: false
+    }];
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(todos(stateBefore, action))
+        .toEqual(stateAfter)
+}
+
+testAddTodo()
 
 console.log('test OK')
 
