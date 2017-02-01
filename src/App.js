51f8ec3ca2065df0abc5a578aff7849f1b61cc7d
Yan Te <yan.te@team.wrike.com>
2017-02-01 15:57:52 +0300

    Combined reducers custom implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
 import deepFreeze from 'deep-freeze';
 import expect from 'expect';
+import { createStore, combineReducers } from 'redux';
+
+
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state
+            }
+            return {
+                ...state,
+                completed: !state.completed
+            }
+        default:
+            return state
+    }
+}
+
 
 const todos = (state = [], action) => {
     switch (action.type) {
     case 'ADD_TODO':
         return  [
             ...state,
-            {
-                id: action.id,
-                text: action.text,
-                completed: false
-            }
+            todo(undefined, action)
         ]
         break;
+    case 'TOGGLE_TODO':
+        return state.map(t => todo(t, action))
     default:
         return state;
 
     }
 }
+
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter
+        default:
+            return state
+    }
+}
+
+
+const todoApp = (state = {}, action) => {
+    return {
+        todos: todos(
+            state.todos,
+            action
+        ),
+        visibilityFilter: visibilityFilter(
+            state.visibilityFilter,
+            action
+        )
+    }
+}
+
+
+const store = createStore(todoApp)
+
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER'
+})
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Something'
+})
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
+
+})
+
+console.log(store.getState());
+
 
 const testAddTodo = () => {
     const stateBefore = [];
@@ -39,6 +107,38 @@ const testAddTodo = () => {
         .toEqual(stateAfter)
 }
 
+const testToggleTodo = () => {
+    const stateBefore = [{
+        id: 0,
+        text: 'Some cool todo',
+        completed: false
+    },{
+        id: 1,
+        text: 'Go shopping',
+        completed: false
+    }]
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    }
+    const stateAfter = [{
+        id: 0,
+        text: 'Some cool todo',
+        completed: false
+    },{
+        id: 1,
+        text: 'Go shopping',
+        completed: true
+    }]
+
+    deepFreeze(stateBefore)
+    deepFreeze(action)
+
+    expect(todos(stateBefore, action))
+        .toEqual(stateAfter)
+}
+
+testToggleTodo()
 testAddTodo()
 
 console.log('test OK')
