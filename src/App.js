abf908f9d1d25db25ad353e8d3687057744e24da
Yan Te <yan.te@team.wrike.com>
2017-02-02 10:22:00 +0300

    extracting container coponents. lifecycle callbacks, component subscribe and unsubscribe

--- src/App.js
+++ src/App.js
@@ -151,47 +151,69 @@ const TodoList = ({
             />)}
     </ul>
 )
-const FilterLink = ({
-    filter,
-    currentFilter,
+const Link = ({
+    active,
     children,
     onClick
 }) => {
-    if(filter==currentFilter) {
+    if(active) {
         return <span>{children}</span>
     }
     return (
         <a href=""
             onClick={e => {
                 e.preventDefault();
-                onClick(filter)
+                onClick()
             }}>
             {children}
         </a>
     )
 }
-
+class FilterLink extends Component {
+    componentDidMount () {
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate()
+        })
+    }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+    render() {
+        const props = this.props;
+        const state = store.getState();
+        return (
+            <Link
+                active={
+                    props.filter === state.visibilityFilter
+                }
+                onClick={() => {
+                    store.dispatch({
+                        type: 'SET_VISIBILITY_FILTER',
+                        filter: props.filter
+                    })
+                }}
+            >
+                {props.children}
+            </Link>
+        );
+    }
+}
 const Footer = (
-    visibilityFilter,
-    onFilterClick
 ) => (
     <footer>
         <FilterLink
             filter='SHOW_ALL'
-            currentFilter={visibilityFilter}
-            onClick={onFilterClick}>
+            >
             All
         </FilterLink>
         <FilterLink
             filter='SHOW_ACTIVE'
-            currentFilter={visibilityFilter}
-            onClick={onFilterClick}>
+            >
             Active
         </FilterLink>
         <FilterLink
             filter='SHOW_COMPLETED'
-            currentFilter={visibilityFilter}
-            onClick={onFilterClick}>
+            >
             Completed
         </FilterLink>
     </footer>
@@ -231,8 +253,8 @@ const App = ({
                     type: 'SET_VISIBILITY_FILTER',
                     filter
                 })
-            }}
-            />
+            }
+        }/>
     </div>
 )
 
